9c338f22e7957d681c03f33b18e6a12cb14ac9a1
Miruna Chisluca <68232365+miruna-chisluca@users.noreply.github.com>
2021-06-27 16:26:45 +0300

    changed api url to deployed app

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://localhost:44326/'
+  // baseURL: 'https://localhost:44326/'
+  baseURL: 'https://biolicious-api.azurewebsites.net/'
 });
 
 export { axiosInstance };
